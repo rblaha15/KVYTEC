f53b4d889472350ca5d8df6c56200049cc349106
rblaha15 <72446087+rblaha15@users.noreply.github.com>
2024-09-29 12:36:41 +0200

    Abstracted sanitization logic

--- backend/src/main/kotlin/cz/regulus/Application.kt
+++ backend/src/main/kotlin/cz/regulus/Application.kt
@@ -6,7 +6,6 @@ import io.ktor.server.netty.*
 import io.ktor.server.request.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
-import io.ktor.utils.io.core.*
 import kotlinx.serialization.encodeToString
 import kotlinx.serialization.json.Json
 import org.apache.poi.ss.usermodel.CellType
@@ -18,57 +17,104 @@ import kotlin.math.roundToLong
 
 fun main(args: Array<String>) = EngineMain.main(args)
 
-enum class Type {
-    String, Number
+fun <T> T.toResult() =
+    Type.SanitizationResult.Succeeded(this)
+
+fun <T> T.toResult(reason: String, isValid: (T) -> Boolean) =
+    if (isValid(this)) Type.SanitizationResult.Succeeded(this)
+    else Type.SanitizationResult.Failed(reason)
+
+fun <T> T?.toResult(reason: String): Type.SanitizationResult<out T> =
+    if (this != null) Type.SanitizationResult.Succeeded(this)
+    else Type.SanitizationResult.Failed(reason)
+
+fun <T, U> Type.SanitizationResult<out T>.withValue(modify: T.() -> Type.SanitizationResult<out U>) =
+    when (this) {
+        is Type.SanitizationResult.Failed -> this
+        is Type.SanitizationResult.Succeeded -> value.modify()
+    }
+
+sealed interface Type<T> {
+    fun sanitize(input: String): SanitizationResult<out T>
+
+    sealed interface SanitizationResult<T> {
+        data class Failed(
+            val reason: String,
+        ) : SanitizationResult<Nothing>
+
+        data class Succeeded<T>(
+            val value: T,
+        ) : SanitizationResult<T>
+    }
+
+    data object Number : Type<Double> {
+        override fun sanitize(input: String) =
+            input.toDoubleOrNull().toResult("Not a valid number").withValue {
+                toResult("Number exceeds, maximum value") { it <= MAX_DOUBLE_VALUE }
+            }
+    }
+
+    data object Text : Type<String> {
+        override fun sanitize(input: String) =
+            input.take(MAX_STRING_LENGTH).replace("[^\\p{L}\\p{N}\\s]".toRegex(), "").toResult()
+    }
+
+    data class ValueList<U : Type<V>, V>(
+        val allowedValues: List<V>,
+        val type: U,
+    ) : Type<V> {
+        override fun sanitize(input: String) = type.sanitize(input).withValue {
+            toResult("Value not valid") { it in allowedValues }
+        }
+    }
 }
 
 // Define allowed values
 const val MAX_STRING_LENGTH = 30
 const val MAX_DOUBLE_VALUE = 1_000_000.0
 
-val zdroje = listOf("1", "2", "3")
-val cirkulaceTV = listOf("ano", "ne")
-val ohrevy = listOf("kombinovaně i kotlem", "bojlerem")
-val jistice = listOf("3x16A", "3x20A", "3x25A")
-val tc = listOf(
-    "RTC 6i", "RTC 13e", "RTC 20e",
-    "EcoAir 614M", "EcoAir 622M", "EcoPart 612M",
-    "EcoPart 616M", "EcoAir 406", "EcoAir 408",
-    "EcoAir 410", "EcoAir 415", "EcoAir 420",
-    "EcoPart 406", "EcoPart 408", "EcoPart 410",
-    "EcoPart 412", "EcoPart 414", "EcoPart 417"
+val puvodniZdrojType = Type.ValueList(listOf(1.0, 2.0, 3.0), Type.Number)
+val cirkulaceTVType = Type.ValueList(listOf("ano", "ne"), Type.Text)
+val zpusobOhrevuType = Type.ValueList(listOf("kombinovaně i kotlem", "bojlerem"), Type.Text)
+val jisticType = Type.ValueList(listOf("3x16A", "3x20A", "3x25A"), Type.Text)
+val tcType = Type.ValueList(
+    listOf(
+        "RTC 6i", "RTC 13e", "RTC 20e",
+        "EcoAir 614M", "EcoAir 622M", "EcoPart 612M",
+        "EcoPart 616M", "EcoAir 406", "EcoAir 408",
+        "EcoAir 410", "EcoAir 415", "EcoAir 420",
+        "EcoPart 406", "EcoPart 408", "EcoPart 410",
+        "EcoPart 412", "EcoPart 414", "EcoPart 417"
+    ), Type.Text
 )
 
-val locations: List<Triple<String, String, Type>> = listOf(
-    Triple("puvodniZdroj", "B13", Type.Number),
-    Triple("cenaZP", "H13", Type.Number),
-    Triple("cenaUhli", "H13", Type.Number),
-    Triple("spotrebaUhli", "H14", Type.Number),
-    Triple("ucinnost", "H15", Type.Number),
-    Triple("zpusobOhrevu", "H16", Type.String),
-    Triple("osob", "B8", Type.Number),
-    Triple("pozadovanaTeplota", "I7", Type.Number),
-    Triple("cirkulaceTV", "I8", Type.String),
-    Triple("jisticPred", "H20", Type.String),
-    Triple("jisticPo", "L20", Type.String),
-    Triple("cenaEeVtPred", "H21", Type.Number),
-    Triple("cenaEeNtPred", "H22", Type.Number),
-    Triple("cenaEeVtPo", "L21", Type.Number),
-    Triple("cenaEeNtPo", "L22", Type.Number),
-    Triple("tepelnaZtrata", "B2", Type.Number),
-    Triple("spotreba", "B9", Type.Number),
-    Triple("venkovniTeplota", "B3", Type.Number),
-    Triple("teplotniSpad", "B4", Type.Number),
-    Triple("tc", "B5", Type.String),
+data class Parameter(
+    val location: String,
+    val name: String,
+    val type: Type<*>,
 )
 
-val validations: Map<String, List<String>> = mapOf(
-    "puvodniZdroj" to zdroje,
-    "zpusobOhrevu" to ohrevy,
-    "jisticPred" to jistice,
-    "jisticPo" to jistice,
-    "cirkulaceTV" to cirkulaceTV,
-    "tc" to tc,
+val parameters: List<Parameter> = listOf(
+    Parameter("B13", "puvodniZdroj", puvodniZdrojType),
+    Parameter("H13", "cenaZP", Type.Number),
+    Parameter("H13", "cenaUhli", Type.Number),
+    Parameter("H14", "spotrebaUhli", Type.Number),
+    Parameter("H15", "ucinnost", Type.Number),
+    Parameter("H16", "zpusobOhrevu", zpusobOhrevuType),
+    Parameter("B8", "osob", Type.Number),
+    Parameter("I7", "pozadovanaTeplota", Type.Number),
+    Parameter("I8", "cirkulaceTV", cirkulaceTVType),
+    Parameter("H20", "jisticPred", jisticType),
+    Parameter("L20", "jisticPo", jisticType),
+    Parameter("H21", "cenaEeVtPred", Type.Number),
+    Parameter("H22", "cenaEeNtPred", Type.Number),
+    Parameter("L21", "cenaEeVtPo", Type.Number),
+    Parameter("L22", "cenaEeNtPo", Type.Number),
+    Parameter("B2", "tepelnaZtrata", Type.Number),
+    Parameter("B9", "spotreba", Type.Number),
+    Parameter("B3", "venkovniTeplota", Type.Number),
+    Parameter("B4", "teplotniSpad", Type.Number),
+    Parameter("B5", "tc", tcType),
 )
 
 val resultLocations: Map<String, String> = mapOf(
@@ -77,17 +123,6 @@ val resultLocations: Map<String, String> = mapOf(
     "nakladyOstatniPred" to "H26",
     "nakladyOstatniPo" to "L26",
 )
-
-fun sanitizeString(value: String) =
-    value.take(MAX_STRING_LENGTH).replace(Regex("[^a-zA-Z0-9\\s]"), "")
-
-fun sanitizeNumber(value: String) =
-    value.toDoubleOrNull()?.takeIf { it <= MAX_DOUBLE_VALUE }
-
-fun validateOption(value: String, options: List<String>, name: String) =
-    (value in options).also {
-        if (!it) println("Invalid value for $name: $value")
-    }
 
 suspend fun RoutingContext.processExcelFile(input: InputStream) {
     val workbook = WorkbookFactory.create(input)
@@ -123,31 +158,25 @@ suspend fun RoutingContext.processExcelFile(input: InputStream) {
     )
 
     // Process incoming values
-    locations.forEach { (name, location, type) ->
+    parameters.forEach { (location, name, type) ->
 
         val rawValue = values[name]
-            ?: return respondBadRequest("\"$name is required but not provided\"")
+            ?: return respondBadRequest("$name: Required but not provided")
 
-        // Validate options for specific fields
-        validations[name]?.let { list ->
-            if (!validateOption(rawValue, list, name)) {
-                return respondBadRequest("\"$name has an invalid value\"")
-            }
-        }
+        val sanitizedValue = when (
+            val result = type.sanitize(rawValue)
+        ) {
+            is Type.SanitizationResult.Failed -> return respondBadRequest(
+                "$name: ${result.reason} ($rawValue)"
+            )
 
-        val sanitizedValue = when (type) {
-            Type.String -> sanitizeString(rawValue)
-            Type.Number -> sanitizeNumber(rawValue)?.toString()
-                ?: return respondBadRequest("\"$name is not a valid number\"")
+            is Type.SanitizationResult.Succeeded -> result.value
         }
 
         // Update the Excel cell with the sanitized value
         val address = CellAddress(location)
         val cell = mainSheet.getRow(address.row).getCell(address.column)
-        when (type) {
-            Type.String -> cell.setCellValue(sanitizedValue)
-            Type.Number -> cell.setCellValue(sanitizedValue.toDouble())
-        }
+        cell.value = sanitizedValue
         evaluator.notifyUpdateCell(cell)
     }
 
